228d6aa6ab8b429153fb9cc04f0d3abba790f73a
lilnug <65525992+Lil-Nug@users.noreply.github.com>
2023-10-12 14:48:23 +0530

    final project
    Used flask to work with GUI

---
+++ final-workex/main.py
@@ -0,0 +1,45 @@
+import mysql.connector
+import flask as f
+from simulation_functions import *
+import time
+
+app = f.Flask(__name__, template_folder='', static_folder='')
+app.config.update(TEMPLATES_AUTO_RELOAD=True)
+app.secret_key='yorealnocapfrfr'
+
+prices = {'hdfc':1232,'uco':5000,'zomato':61,'hindustan_uni':2500,'cipla':920,'tata':450}
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    display=''
+    if f.request.method=='POST':
+        buybtn=f.request.form.get('buy')
+        sellbtn=f.request.form.get('sell')
+        admno= f.request.form["admno"]
+        comp=f.request.form["company"].lower()
+        try:
+            shares=int(f.request.form["noshares"])
+            if buybtn:
+                msg=insert(admno,comp,shares,prices)
+                if msg:
+                    display='not enough money' 
+            elif sellbtn:
+                msg=delete(admno,comp,shares,prices)
+                if msg:
+                    display='selling more than you have'
+        except:
+            display='invalid no of shares'
+        
+    db=mysql.connector.connect(host='localhost', user='root', password='5120', database='workex')
+    cur = db.cursor()
+    cur.execute('select * from students;')
+    data=cur.fetchall()
+    db.close()
+    cur.close()
+    return f.render_template('index.html', data=data,display=display,prices=prices)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
---
+++ final-workex/simulation_functions.py
@@ -0,0 +1,46 @@
+import mysql.connector
+def insert(adno,company,amount,prices):
+    try:
+        db=mysql.connector.connect(host='localhost', user='root', password='5120', database='workex')
+        mc= db.cursor()
+        mc.execute(f'select cash_in_hand,{company} from students where admission_no={adno}')
+        fetch= mc.fetchall()
+        mc.execute(f'select cash_in_hand,{company} from students where admission_no={adno}')
+        fetch= mc.fetchall()
+
+        if fetch==[]:
+            mc.execute(f"insert into students (admission_no,cash_in_hand,{company}) values ({adno},{5000-(prices[company]*amount)},{amount})")
+
+        else:
+            cash=fetch[0][0]
+            comp=fetch[0][1]
+            mc.execute(f"update students set cash_in_hand={cash-prices[company]*amount},{company}={comp+amount} where admission_no={adno}")
+        db.commit()
+        db.close()
+    except Exception as e:
+        db.close()
+
+        return e
+
+
+
+
+def delete(adno,company,amount,prices):
+    try:
+        db=mysql.connector.connect(host='localhost', user='root', password='5120', database='workex')
+        mc= db.cursor()
+        mc.execute(f'select cash_in_hand,{company} from students where admission_no={adno}')
+        fetch= mc.fetchall()
+
+        if fetch==[]:
+            return 'bro does not exist'
+        else:
+            cash=fetch[0][0]
+            comp = fetch[0][1]
+            mc.execute(f"update students set cash_in_hand={cash+prices[company]*amount},{company}={comp-amount} where admission_no={adno}")
+        db.commit()
+        db.close()
+    except Exception as e:
+        db.close()
+        print(e)
+        return e
